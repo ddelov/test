7d5600aef2bac72b06844a97bf099eb2b1c75550
Delcho Delov <delcho.delov@gmail.com>
2018-07-09 15:48:17 +0300

    Added Pi evaluation for Spark distributed cluster

--- src/main/java/com/estafet/spark/SparkPiBootApplication.java
+++ src/main/java/com/estafet/spark/SparkPiBootApplication.java
@@ -1,7 +1,7 @@
 package com.estafet.spark;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
